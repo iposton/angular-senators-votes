b2c53297eb8cb97b14b13a3ef42480c8d34281e4
iposton <ianposton@sbcglobal.net>
2017-04-04 10:35:37 -0700

    new design and color put overlay on senator img according to party

--- client/app/components/content/content-directive.js
+++ client/app/components/content/content-directive.js
@@ -205,5 +205,14 @@
 
 
             }
-        })
+        }).directive('parseStyle', function($interpolate) {
+    return function(scope, elem) {
+        var exp = $interpolate(elem.html()),
+            watchFunc = function () { return exp(scope); };
+
+        scope.$watch(watchFunc, function (html) {
+            elem.html(html);
+        });
+    };
+});
 }());
